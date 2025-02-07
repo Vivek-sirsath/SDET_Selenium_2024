c84be811f37b009cd8a331f1b673dc05c9d7df14
vivek <vivek.sirsath1@gmail.com>
2025-02-08 00:23:12 +0530

    Spelling corrected - 'Handling'

--- SDET_Selenium_2024/src/test/java/brokenLinks/HandlingBrokenLinks.java
+++ SDET_Selenium_2024/src/test/java/brokenLinks/HandlingBrokenLinks.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class HandleingBrokenLinks {
+public class HandlingBrokenLinks {
 
 	public static void main(String[] args) throws MalformedURLException, IOException {
 		

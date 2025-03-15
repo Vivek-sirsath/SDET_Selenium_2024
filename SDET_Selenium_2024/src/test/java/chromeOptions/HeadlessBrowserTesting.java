4de90760c1512af1a85711a8fba48a220b3f3ec4
vivek <vivek.sirsath1@gmail.com>
2025-03-15 21:55:03 +0530

    chromeOptions > HeadlessBrowserTesting.java added

---
+++ SDET_Selenium_2024/src/test/java/chromeOptions/HeadlessBrowserTesting.java
@@ -0,0 +1,33 @@
+package chromeOptions;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class HeadlessBrowserTesting {
+
+	public static void main(String[] args) {
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new"); // setting for headless mode of execution
+		
+		WebDriver driver = new ChromeDriver(options);
+		// We've to pass the options here, else the browser will launch
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+		String pageTitle = driver.getTitle();
+		
+		if(pageTitle.equals("Automation Testing Practice")) {
+			System.out.println("Title test passed");
+		} else {
+			System.out.println("Title test failed");
+		}
+
+	}
+
+}

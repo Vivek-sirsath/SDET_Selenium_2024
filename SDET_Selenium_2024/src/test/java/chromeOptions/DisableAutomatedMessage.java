4df1a47090161ae0b7df570cd48d01d3e3f0cf86
vivek <vivek.sirsath1@gmail.com>
2025-03-15 21:51:03 +0530

    chromeOptions > DisableAutomatedMessage.java added

---
+++ SDET_Selenium_2024/src/test/java/chromeOptions/DisableAutomatedMessage.java
@@ -0,0 +1,24 @@
+package chromeOptions;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DisableAutomatedMessage {
+
+	public static void main(String[] args) {
+		
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("excludeSwitches", new String[] {"enable-automation"});
+		
+		WebDriver driver = new ChromeDriver(options);
+		// We've to pass the options here, else the browser will launch
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://testautomationpractice.blogspot.com/");
+
+	}
+
+}

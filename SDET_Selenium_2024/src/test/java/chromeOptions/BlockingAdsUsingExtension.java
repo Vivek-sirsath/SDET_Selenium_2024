40a2f982e079549dfffb5ff8d441aca4b0233b28
vivek <vivek.sirsath1@gmail.com>
2025-04-27 00:27:09 +0530

    Ad blocking using extensions in Selenium added

--- SDET_Selenium_2024/src/test/java/chromeOptions/BlockingAdsUsingExtension.java
+++ SDET_Selenium_2024/src/test/java/chromeOptions/BlockingAdsUsingExtension.java
@@ -6,24 +6,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BlockingAdsUsingExtension {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		String crxFilePath = "F:\\Automation Testing\\SDET 2024 Java + Selenium\\CRX files for Extensions\\uBlock-Origin-Chrome-Web-Store.crx";
+		 WebDriverManager.chromedriver().setup();
 
-		ChromeOptions options = new ChromeOptions();
+	        ChromeOptions options = new ChromeOptions();
+	        options.addExtensions(new File("F:\\Automation Testing\\CRX Files\\uBlock-Origin-LiteMV3.crx"));
+	        // Manifest V3 is a new architecture for Chrome extensions.
+	       
+	        WebDriver driver = new ChromeDriver(options);
+	        driver.manage().window().maximize();
 
-		File uBlockExtensionCrx = new File(crxFilePath);
+	        Thread.sleep(3000); // wait for extensions to load completely
 
-		options.addExtensions(uBlockExtensionCrx);
-
-		WebDriver driver = new ChromeDriver(options);
-
-		driver.manage().window().maximize();
-
-		driver.get("https://text-compare.com/");
-
+	        driver.get("https://text-compare.com/");
+	        System.out.println("Extension blocked the adds successfully");
 	}
 
 }

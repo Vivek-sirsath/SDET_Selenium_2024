09b033bb4b4af90d32c909ca00b52305e10c1ca8
vivek <vivek.sirsath1@gmail.com>
2025-11-23 17:52:45 +0530

    Conflict resolved in ActionsClassVsActionInterface.java

--- SDET_Selenium_2024/src/test/java/mouseActions/ActionsClassVsActionInterface.java
+++ SDET_Selenium_2024/src/test/java/mouseActions/ActionsClassVsActionInterface.java
@@ -6,16 +6,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-<<<<<<< HEAD
-=======
 import org.openqa.selenium.interactions.Action;
->>>>>>> 8c6d85c (Saving my local changes before pull)
 import org.openqa.selenium.interactions.Actions;
 
 public class ActionsClassVsActionInterface {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
 		WebDriver driver = new ChromeDriver();
 
@@ -32,21 +28,16 @@ public class ActionsClassVsActionInterface {
 	    // No need to store the action in Action interface.
 		
 		System.out.println("Right click button clicked...");
-=======
 		
-WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html");
 		
 		WebElement clickMeBtn = driver.findElement(By.xpath("//span[contains(normalize-space(),'right click me')]"));
-		
-		Actions act = new Actions(driver);
-		
+			
 		Action myAction = act.contextClick(clickMeBtn).build(); // Creating action
 		myAction.perform(); // Completing the action
->>>>>>> 8c6d85c (Saving my local changes before pull)
 
 	}
 

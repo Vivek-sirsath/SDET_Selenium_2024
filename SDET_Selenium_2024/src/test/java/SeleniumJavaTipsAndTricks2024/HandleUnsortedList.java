9691d3793eda01008ae4ce4757e469387dca9817
vivek <vivek.sirsath1@gmail.com>
2025-12-09 08:51:05 +0530

    Handling of both Sorted and Unsorted list added.

---
+++ SDET_Selenium_2024/src/test/java/SeleniumJavaTipsAndTricks2024/HandleUnsortedList.java
@@ -0,0 +1,56 @@
+package SeleniumJavaTipsAndTricks2024;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class HandleUnsortedList {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://testautomationpractice.blogspot.com/");
+				
+        Select drpDown = new Select(driver.findElement(By.xpath("//select[@id='colors']")));
+        
+        ArrayList origList = new ArrayList();
+        ArrayList sortedList = new ArrayList();
+        
+        List <WebElement> options = drpDown.getOptions();
+        
+        for(WebElement option : options) {
+        	origList.add(option.getText());
+        }
+        
+        // Before Sorting
+        System.out.println("Before Sorting...");
+        System.out.println("Original List: " + origList);
+        
+        Collections.sort(origList);
+        
+        for(WebElement option : options) {
+        	sortedList.add(option.getText());
+        }
+        
+        // After Sorting
+        System.out.println("After Sorting...");
+        System.out.println("Original List: " + origList);
+        
+        if(origList.equals(sortedList)) {
+        	System.out.println("List box is sorted...");
+        }else {
+        	System.out.println("List box is not sorted");
+        }
+        
+        driver.quit();
+
+	}
+
+}
